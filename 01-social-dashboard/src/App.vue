9387b1f1eae065b08d630d9b38e980e108bebae5
Murilo <muriloholiveira@hotmail.com>
2020-08-17 23:21:41 -0300

    :sparkles: Styling Component

--- 01-social-dashboard/src/App.vue
+++ 01-social-dashboard/src/App.vue
@@ -1,21 +1,66 @@
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <BigCard
+      socialIcon="icon-facebook.svg"
+      socialName="Zé da Lambreta"
+      socialValue="1000"
+      socialType="FOLLOWERS"
+      arrowIcon="icon-down.svg"
+      todayValue="99"
+    />
+    <BigCard
+      socialIcon="icon-twitter.svg"
+      socialName="Zé da Biz"
+      socialValue="5000"
+      socialType="FOLLOWERS"
+      arrowIcon="icon-up.svg"
+      todayValue="30"
+    />
+    <BigCard
+      socialIcon="icon-instagram.svg"
+      socialName="Zé da Hornet"
+      socialValue="10000"
+      socialType="FOLLOWERS"
+      arrowIcon="icon-up.svg"
+      todayValue="3"
+    />
+    <BigCard
+      socialIcon="icon-youtube.svg"
+      socialName="Zé da Twister"
+      socialValue="500"
+      socialType="SUBSCRIBERS"
+      arrowIcon="icon-down.svg"
+      todayValue="100"
+    />
+  </div>
 </template>
 
 <script>
+import BigCard from "./components/BigCard.vue";
 export default {
   name: "App",
-  components: {},
+  components: {
+    BigCard,
+  },
 };
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  outline: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background-color: hsl(230, 17%, 14%);
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-around;
 }
 </style>
--- 01-social-dashboard/src/components/BigCard.vue
+++ 01-social-dashboard/src/components/BigCard.vue
@@ -1,10 +1,72 @@
 <template>
-  <div></div>
+  <div class="container">
+    <div class="social-info">
+      <img v-bind:src="require(`../assets/icons/${socialIcon}`)" alt="Social Logo" />
+      <p>{{socialName}}</p>
+    </div>
+    <div class="values-info">
+      <h1>{{socialValue}}</h1>
+      <p>{{socialType}}</p>
+    </div>
+    <div class="today-info">
+      <img v-bind:src="require(`../assets/icons/${arrowIcon}`)" />
+      <p>{{todayValue}}</p>
+      <p>Today</p>
+    </div>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "BigCard",
+  props: {
+    socialIcon: { type: String },
+    socialName: { type: String },
+    socialValue: { type: Number },
+    socialType: { type: String },
+    arrowIcon: { type: String },
+    todayValue: { type: Number },
+  },
+};
 </script>
 
 <style scoped>
+.container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  background-color: hsl(228, 28%, 20%);
+}
+
+.container .social-info {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
+
+.container .social-info p {
+  color: hsl(228, 34%, 66%);
+}
+
+.container .values-info {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.container .values-info h1 {
+  color: hsl(0, 0%, 100%);
+}
+
+.container .values-info p {
+  color: hsl(228, 34%, 66%);
+}
+
+.container .today-info {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+}
 </style>
